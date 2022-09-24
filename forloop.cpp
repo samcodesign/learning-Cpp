b84f154fc11b585dd4c041842fea0e47af533a80
samcodesign <samyben152@gmail.com>
2022-09-24 22:39:48 +0100

    adding things

---
+++ forloop.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+    int nums[]= {1,2,3,4,5,6,7};
+
+    for (int i = 0; i < 7; i++)
+    {
+        cout << nums[i]<< endl;
+    }
+    
+    return 0;
+}
